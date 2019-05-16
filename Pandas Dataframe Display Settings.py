e6f1c1f2fd168fb8559c09c85ad0af5b97fbf1fa
Aaron Horvitz <AaronNHorvitz@users.noreply.github.com>
2019-05-16 11:39:40 -0500

    Add files via upload
    When writing df.head() to display pandas data, it's annoying to see the data bunched up.
    This code, when written in a Jupyter notebook, will hopefully alleviate some of that concern.

---
+++ Pandas Dataframe Display Settings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 11:33:46 2019
+
+@author: ahorvitz
+"""
+"""
+When writing df.head() to display pandas data, it's annoying to see the data bunched up.
+This code, written in a jupyter notebook, will hopefully aleviate some of that concern. 
+"""
+
+import pandas as pd
+pd.options.display.max_rows = 100        #Increases the number of rows displayed without a break in the middle.
+pd.options.display.max_columns = 100     #Increases the number of columns displayed without a break in the middle.
+pd.set_option('max_colwidth',100)        #Increases the width of the columns to display 100 characters at the same time.
