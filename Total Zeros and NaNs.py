5ef9a2a8b7be95300d0fe24697e25ef69e1c2426
Aaron Horvitz <AaronNHorvitz@users.noreply.github.com>
2019-08-16 14:54:52 -0500

    Update Total Zeros and NaNs.py

--- Total Zeros and NaNs.py
+++ Total Zeros and NaNs.py
@@ -5,8 +5,8 @@ def total_zeros(df):      #Returns a data frame with the total number of zeros i
     columns_list = df.columns.tolist()
     df = df[-1:]
 #    df = df.astype(int)                                                    #Use this line of code to convert values to int
-#    df.head()                                                              #Use this line of code to display the totals
-    return(df)                                                             #View the total numbers of NaN values by each column.
+    df.head()                                                              #Use this line of code to display the totals
+    return(df)                                                             #View the total number of Zeros values by each column.
 
 def total_nans(df):      #Returns a data frame with the total number of NaNs in each column of data. 
 
@@ -16,5 +16,4 @@ def total_nans(df):      #Returns a data frame with the total number of NaNs in 
     df = df[-1:]
 #    df = df.astype(int)                                                    #Use this line of code to convert values to int.
     df.head()                                                               #use this line of code to display the totals
-    return(df)                                                             #View the total numbers of NaN values by each column.
-    
+    return(df)                     
