120ab8dbbc6182b0e7a35e459a29da77db0c9d77
Aaron Horvitz <AaronNHorvitz@users.noreply.github.com>
2019-08-16 14:53:41 -0500

    Update Total Zeros and NaNs.py

--- Total Zeros and NaNs.py
+++ Total Zeros and NaNs.py
@@ -10,7 +10,7 @@ def total_zeros(df):      #Returns a data frame with the total number of zeros i
 
 def total_nans(df):      #Returns a data frame with the total number of NaNs in each column of data. 
 
-    df.loc['Total NaNs']   = .isnull().sum()                              #Create a 'Total NaNs' row on the bottom of the data frame
+    df.loc['Total NaNs']   = df.isnull().sum()                              #Create a 'Total NaNs' row on the bottom of the data frame
     df = df.sort_values(by = 'Total NaNs', axis = 1, ascending = False)   #Order the data frame to look at least to greatest
     columns_list = df.columns.tolist()
     df = df[-1:]
